070dbec57b821559d40f4661ceccb290dd878de7
anras5 <anras1filip@gmail.com>
2022-02-21 19:05:25 +0100

    Add set and unset

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, CallbackContext, Filters
 
+from olx_notifier import message_maker
 from data_handler import DataHandler
 
 load_dotenv()
 
-API_KEY = os.environ.get('PEEPOLEAVEBOT_API_KEY')
+API_KEY = os.environ.get('API_KEY')
 DEVELOPER_CHAT_ID = os.environ.get('DEVELOPER_CHAT_ID')
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -18,6 +19,7 @@ logger = logging.getLogger(__name__)
 data_handler = DataHandler()
 
 NAME, LINK = 0, 1
+SECONDS = 10
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -78,7 +80,7 @@ def add_link(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 
-def cancel(update: Update, context: CallbackContext):
+def cancel(update: Update, context: CallbackContext) -> int:
     """Cancels and ends the conversation."""
     update.message.reply_text('Okay. Back to the main menu. Press /start to begin.', parse_mode='HTML')
 
@@ -94,12 +96,70 @@ def delete(update: Update, context: CallbackContext):
     update.message.reply_text(text, parse_mode='MarkdownV2')
 
 
+def set_timer(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+
+    try:
+        due = SECONDS
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_repeating(alarm, interval=due, context=chat_id, name=str(chat_id))
+
+        text = 'Timer successfully set!'
+        if job_removed:
+            text += ' Old one was removed.'
+        update.message.reply_text(text)
+
+    except (IndexError, ValueError):
+        update.message.reply_text(f'Sorry :( an error occured. Try again.')
+
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+def alarm(context: CallbackContext) -> None:
+    """Send the alarm message."""
+    job = context.job
+    chat_id = job.context
+    text = message_maker(chat_id)
+    if text:
+        context.bot.send_message(job.context, text=text)
+
+
+def unset(update: Update, context: CallbackContext) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
+    update.message.reply_text(text)
+
+
+def user_data(update: Update, context: CallbackContext):
+    """Allows the user to read their data"""
+    chat_id = update.message.chat_id
+
+    data = data_handler.get_data_by_id(chat_id)
+    text = ""
+    for name, values in data.items():
+        text += f'{name} -> {values.get("Number")}\n'
+    update.message.reply_text(f"Your data:\n{text}")
+
+
 def main() -> None:
     updater = Updater(API_KEY)
 
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', start))
     dispatcher.add_handler(ConversationHandler(
         entry_points=[CommandHandler('add', add)],
         states={
@@ -107,9 +167,10 @@ def main() -> None:
             LINK: [MessageHandler(Filters.regex(r"^(https:\/\/.*olx.pl\/.*|no link)$"), add_link)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]))
-
-    # dispatcher.add_handler(CommandHandler("set", set_timer))
-    # dispatcher.add_handler(CommandHandler("unset", unset))
+    dispatcher.add_handler(CommandHandler('delete', delete))
+    dispatcher.add_handler(CommandHandler("set", set_timer))
+    dispatcher.add_handler(CommandHandler("unset", unset))
+    dispatcher.add_handler(CommandHandler('mydata', user_data))
 
     updater.start_polling()
     updater.idle()
---
+++ olx_notifier.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+from data_handler import DataHandler
+
+
+def olx_checker(url):
+    """This function returns current number of offers for a URL"""
+    headers_mobile = {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X)'
+                      ' AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B137 Safari/601.1'}
+    response = requests.get(url, headers=headers_mobile)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    offer = soup.find(name="span", class_="css-xkzsoo")
+    if offer is not None:
+        number_of_offers = int(offer.getText().split()[1])
+        return number_of_offers
+    else:
+        return 0
+
+
+def message_maker(user_id):
+    """This function creates a message that is supposed to be send.
+     It checks if the previous number of offers for an item is lower than the current one.
+     If so, it concatenates new info to the 'content' string which is returned at the end.
+     It contains all the valuable information and will be send as the message to the user."""
+
+    data_handler = DataHandler()
+    data = data_handler.get_data_by_id(user_id=user_id)
+
+    content = ""
+
+    for name, values in data.items():
+        previous_number_offer = values["Number"]
+        url = values["Url"]
+        current_number_offer = olx_checker(url=url)
+        if current_number_offer > previous_number_offer:
+            content += f"New offers {name}. " \
+                       f"Difference {current_number_offer - previous_number_offer} " \
+                       f"URL: {url}\n"
+        values["Number"] = current_number_offer
+
+    data_handler.update_user_data(user_id, new_data=data)
+
+    return content
